e8d0f4794a4360fc4a7842c04f33c67d488f5535
danielanthonyl <danskyjs@icloud.com>
2022-08-10 19:54:05 -0300

    fix (ui): removed input from app.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,8 @@
-import { Input } from './components/Input';
 import { Layout } from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Input placeholder="hey ho" />
-
       <Layout></Layout>
     </div>
   );
