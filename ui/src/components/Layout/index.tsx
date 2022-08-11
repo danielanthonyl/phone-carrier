484ea644467d52c45f2293b867d2029bde3f6cc3
danielanthonyl <danskyjs@icloud.com>
2022-08-11 17:26:52 -0300

    feature (ui): update styles

--- ui/src/components/Layout/index.tsx
+++ ui/src/components/Layout/index.tsx
@@ -3,7 +3,7 @@ import { ILayoutProps } from './types';
 import classes from './styles.module.scss';
 
 export const Layout = ({ children }: ILayoutProps) => (
-  <div>
+  <div className={classes.app}>
     <Header />
     <main className={classes.main}>{children}</main>
     <Footer email="contato@telzir.com.br" phone="+55 19 0000-0000" />
